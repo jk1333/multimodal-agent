2cc7d7cc050cdfab79572f900f887bedb2503d6b
jk1333 <jk13@nate.com>
2026-06-22 17:10:51 +0900

    Add indexer tutorial

--- app/common.py
+++ app/common.py
@@ -53,3 +53,4 @@ logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
 )
 logger = logging.getLogger(__name__)
+logging.getLogger("httpx").setLevel(logging.WARNING)
